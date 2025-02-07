512faf8b455cd92c2dfff975edaaf3811d57c39f
Arihant1208 <arihantjain1208@gmail.com>
2025-02-07 08:14:19 -0800

    Borrowing error fixed  in generics

--- src/generics.rs
+++ src/generics.rs
@@ -28,23 +28,27 @@ struct Point2<X1, Y1> {
     y: Y1,
 }
 
-// impl<X1, Y1> Point2<X1, Y1> {
-//     fn mixup<X2, Y2>(self, other: &Point2<X2, Y2>) -> Point2<X1, Y2> {
-//         Point2 {
-//             x: self.x,
-//             y: other.y,
-//         }
-//     }
-// }
+impl<X1, Y1> Point2<X1, Y1> {
+    fn mixup<X2, Y2>(&self, other: &Point2<X2, Y2>) -> Point2<X1, Y2> 
+    where
+        X1: Clone, // Ensure X1 can be cloned
+        Y2: Clone, // Ensure Y2 can be cloned
+    {
+        Point2 {
+            x: self.x.clone(), // Clone X1
+            y: other.y.clone(), // Clone Y2
+        }
+    }
+}
 
-// pub fn genericsImpl_2(){
-//     let obj = Point2{x:1.89,y:20};
+pub fn genericsImpl_2(){
+    let obj = Point2{x:1.89,y:20};
 
-//     let obj2 = Point2{x:1.89,y:2.19};
+    let obj2 = Point2{x:1.89,y:2.19};
 
-//     let finalObj = &obj.mixup(&obj2);
+    let finalObj = obj.mixup(&obj2);
 
-//     println!("obj1 = {obj:?} , obj2 = {obj2:?} and final result = {finalObj:?}")
-// }
+    println!("obj1 = {obj:?} , obj2 = {obj2:?} and final result = {finalObj:?}")
+}
 
 //////////////// NEED TO RESOLVE THIS ERROR !!!!!!!!!! ///////////////////////////////////////////////////////
